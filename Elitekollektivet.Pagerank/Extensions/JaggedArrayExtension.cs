14b95adde4e8919a6764d681e0b5605994197d1e
Andreas Isnes Nilsen <andreas.nilsen@sparebank1.no>
2020-07-16 17:31:02 +0200

    renamed extensions to extension

---
+++ Elitekollektivet.Pagerank/Extensions/JaggedArrayExtension.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elitekollektivet.Pagerank.Extensions
+{
+    public static class JaggedArrayExtension
+    {
+        public static T[,] ToMultidimensional<T>(this T[][] source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            T[,] result = new T[source.Length, source[0].Length];
+            Parallel.ForEach(Enumerable.Range(0, source.Length), i =>
+            {
+                for (int j = 0; j < source[0].Length; j++)
+                {
+                    result[i, j] = source[i][j];
+                }
+            });
+
+            return result;
+        }
+    }
+}
